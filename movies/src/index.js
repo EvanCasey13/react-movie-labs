bdc674e4440685f6b3e09a875f881d971fc95ea8
EvanCasey13 <evancasey2003@gmail.com>
2022-10-21 19:11:20 +0100

    Using react-query to manage the browser-based cache.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
@@ -7,9 +6,24 @@ import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader'
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { createRoot } from "react-dom/client";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
+  
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <SiteHeader />
     <Routes>
@@ -21,7 +35,9 @@ const App = () => {
       <Route path="*" element={ <Navigate to="/" /> } />
     </Routes>
   </BrowserRouter>
+  <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
   );
 };
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = createRoot(  document.getElementById("root") )
+rootElement.render(<App />);
